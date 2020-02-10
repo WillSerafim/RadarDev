ff393763dfb10dac1daf4a4f0d9aabb98ccf0002
Willian Serafim <willian_50@ymail.com>
2020-02-09 21:28:16 -0300

    Implementação de WebSocket

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMenssage } = require('../websocket');
 
 /*  
     controller methods
@@ -40,6 +41,16 @@ module.exports = {
                 techs: techsArray,
                 location, 
             })
+
+            //Filtra as conexões que estão há no máximo 10km de distancia
+            //e que o dev novo tenha pelo menos uma das techs filtradas
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray, 
+            )
+
+            sendMenssage(sendSocketMessageTo, 'new-dev', dev);
         }
     
         return response.json(dev);
